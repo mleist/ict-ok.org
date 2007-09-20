5b9c232efa1ffad18a5e7a143f0233fe099f8657
Markus Leist <markus@lei.st>
2007-09-20 17:07:25 +0000

    new Ring-Buffer
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@48 befa3f1b-49e7-4dde-a7c4-38a0be74952b

--- trunk/org/ict_ok/libs/lib.py
+++ trunk/org/ict_ok/libs/lib.py
@@ -87,3 +87,27 @@ def convertSystemUptime2String(uptime_sec):
     else: upstr = "%d days, " % stru[0]
     upstr += "%02d:%02d:%02d" % (stru[1], stru[2], stru[3])
     return upstr
+
+class RingBuffer:
+    def __init__(self,size_max):
+        self.max = size_max
+        self.data = []
+    def append(self,x):
+        """append an element at the end of the buffer"""
+        self.data.append(x)
+        if len(self.data) == self.max:
+            self.cur=0
+            self.__class__ = RingBufferFull
+    def get(self):
+        """ return a list of elements from the oldest to the newest"""
+        return self.data
+
+
+class RingBufferFull:
+    def __init__(self,n):
+        raise "you should use RingBuffer"
+    def append(self,x):             
+        self.data[self.cur]=x
+        self.cur=(self.cur+1) % self.max
+    def get(self):
+        return self.data[self.cur:]+self.data[:self.cur]
