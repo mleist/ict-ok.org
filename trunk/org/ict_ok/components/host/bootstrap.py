033e7c81476b23ae125af15486cc0fcd7492e667
Markus Leist <markus@lei.st>
2007-12-29 17:03:44 +0000

    new bootstrap function to create a 'local system' entry on first startup
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@115 befa3f1b-49e7-4dde-a7c4-38a0be74952b

--- trunk/org/ict_ok/components/host/bootstrap.py
+++ trunk/org/ict_ok/components/host/bootstrap.py
@@ -7,7 +7,7 @@
 #
 # $Id$
 #
-# pylint: disable-msg=E1101
+# pylint: disable-msg=E1101,E0611
 #
 """startup of subsystem"""
 
@@ -16,6 +16,7 @@ __version__ = "$Id$"
 # phython imports
 import logging
 import transaction
+from datetime import datetime
 
 # zope imports
 from zope.app.appsetup import appsetup
@@ -23,11 +24,34 @@ from zope.app.appsetup.bootstrap import getInformationFromEvent
 from zope.app.catalog.field import FieldIndex
 from zope.app.catalog.interfaces import ICatalog
 from zope.index.text.interfaces import ISearchableText
+from zope.dublincore.interfaces import IZopeDublinCore
+from zope.lifecycleevent import ObjectCreatedEvent
+from zope.event import notify
+from zope.component import createObject
 
 # ict_ok.org imports
 from org.ict_ok.admin_utils.supervisor.interfaces import IAdmUtilSupervisor
 
 logger = logging.getLogger("Compon. Host")
+
+def createLocalSystem(root_folder):
+    dateNow = datetime.utcnow()
+    #import pdb;pdb.set_trace()
+    newHost = createObject(u'org.ict_ok.components.host.host.Host')
+    notify(ObjectCreatedEvent(newHost))
+    root_folder.__setitem__(newHost.getObjectId(), 
+                            newHost)
+    dcore = IZopeDublinCore(newHost, None)
+    dcore.creators = [u'first bootstrap']
+    newHost.ikComment += u"bootstrap on %s" % (dateNow)
+    newHost.__setattr__("ikName", u'Local System')
+    newHost.__setattr__("hostname", u'Local System')
+    dcore.title = u'Local System'
+    newHost.__setattr__("ikDesc", u'this is the ict-ok.org management-system')
+    newHost.__setattr__("manufacturer", u'see ict-ok.org')
+    newHost.__setattr__("vendor", u'see ict-ok.org')
+    newHost.__setattr__("genNagios", True)
+    dcore.created = dateNow
 
 def bootStrapSubscriber(event):
     """initialisation of IntId utility on first database startup
@@ -65,5 +89,8 @@ def bootStrapSubscriber(event):
         instAdmUtilSupervisor.appendEventHistory(\
             u" bootstrap: ICatalog - create index for entry type 'host'")
 
+    # creates and stores the local system in ZODB
+    # createLocalSystem(root_folder)
+    
     transaction.get().commit()
     connection.close()
