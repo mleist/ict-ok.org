061e347def5f68e9faafc5530e952f772e34c4a1
Markus Leist <markus@lei.st>
2008-02-15 20:21:56 +0000

    ict_ok.net now supports 'eventInp_inward_relaying_shutdown'
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@143 befa3f1b-49e7-4dde-a7c4-38a0be74952b

--- trunk/org/ict_ok/components/net/interfaces.py
+++ trunk/org/ict_ok/components/net/interfaces.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2004, 2005, 2006, 2007,
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
 #               Markus Leist <leist@ikom-online.de>
 # See also LICENSE.txt or http://www.ict-ok.org/LICENSE
 # This file is part of ict-ok.org.
@@ -14,12 +14,15 @@
 __version__ = "$Id$"
 
 # zope imports
+from zope.schema import Choice, Set
 from zope.i18nmessageid import MessageFactory
 from zope.app.container.constraints import contains
 
 # ict_ok.org imports
 from org.ict_ok.components.interfaces import IComponent
-from org.ict_ok.schema.ipvalid import IpValid
+from org.ict_ok.components.supernode.interfaces import \
+     IEventIfSupernode
+from org.ict_ok.schema.ipvalid import NetIpValid
 
 _ = MessageFactory('org.ict_ok')
 
@@ -29,7 +32,7 @@ class INet(IComponent):
 
     contains('org.ict_ok.components.host.interfaces.IHost')
 
-    ipv4 = IpValid(
+    ipv4 = NetIpValid(
         min_length = 1,
         max_length = 30,
         title = _("Network IP"),
@@ -40,4 +43,22 @@ class INet(IComponent):
     
     def containsIp(ipString):
         """ is ip(String) part of this network?
+        """
+
+
+class IEventIfEventNet(IEventIfSupernode):
+    """ event interface of object """
+    
+    eventInpObjs_inward_relaying_shutdown = Set(
+        title = _("inward relaying shutdown <-"),
+        value_type = Choice(
+            title = _("objects"),
+            vocabulary="AllEventInstances"),
+        default = set([]),
+        readonly = False,
+        required = True)
+    
+    def eventInp_inward_relaying_shutdown(eventMsg):
+        """
+        forward the event to all objects in this container through the signal filter
         """
--- trunk/org/ict_ok/components/net/net.py
+++ trunk/org/ict_ok/components/net/net.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2004, 2005, 2006, 2007,
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
 #               Markus Leist <leist@ikom-online.de>
 # See also LICENSE.txt or http://www.ict-ok.org/LICENSE
 # This file is part of ict-ok.org.
@@ -29,7 +29,7 @@ from zope.app.intid.interfaces import IIntIds
 # ict_ok.org imports
 from org.ict_ok.schema.IPy import IP
 from org.ict_ok.components.component import Component
-from org.ict_ok.components.net.interfaces import INet
+from org.ict_ok.components.net.interfaces import INet, IEventIfEventNet
 
 
 class Net(Component):
@@ -37,24 +37,24 @@ class Net(Component):
     the template instance
     """
 
-    implements(INet)
+    implements(INet, IEventIfEventNet)
     # for ..Contained we have to:
     __name__ = __parent__ = None
     ipv4 = FieldProperty(INet['ipv4'])
+
+    eventInpObjs_inward_relaying_shutdown = FieldProperty(\
+        IEventIfEventNet['eventInpObjs_inward_relaying_shutdown'])
 
     def __init__(self, **data):
         """
         constructor of the object
         """
         Component.__init__(self, **data)
-        # find our correct factory, is there a better solution?
-        for (fact_name, fact_obj) in zapi.getFactoriesFor(INet):
-            if (len(fact_name) > 11) and (fact_name[:11]=='org.ict_ok.'):
-                self.myFactory = unicode(fact_name)
         print "INet.names(): ", list(INet.names())
         for (name, value) in data.items():
             if name in INet.names():
                 setattr(self, name, value)
+        self.eventInpObjs_inward_relaying_shutdown = set([])
         self.ikRevision = __version__
 
     def containsIp(self, ipString):
@@ -63,6 +63,26 @@ class Net(Component):
         myIp = IP(self.ipv4)
         testIp = IP(ipString)
         return testIp in myIp
+
+    def eventInp_inward_relaying_shutdown(self, eventMsg=None):
+        """
+        forward the event to all objects in this container through the signal filter
+        """
+        #eventMsg.stopit(self, "Net.eventInp_inward_relaying_shutdown")
+        print "Net.eventInp_inward_relaying_shutdown (%s)       " \
+        "       ############## <-" % (self.ikName)
+        for name, obj in self.items():
+            print "-> ", obj
+            try:
+                obj.eventInp_shutdown(eventMsg)
+            except AttributeError:
+                print "Dont find method"
+            #for attrName in obj.__dict__:
+                #print "attr:   %s" % (attrName)
+                #if attrName.find("eventInp_shutdown") == 0: # attribute name starts with ...
+                    #fnct = getattr(obj, "eventInp_shutdown", None)
+                    #if fnct is not None:
+                        #fnct(eventMsg)
 
 def getAllNetworks():
     """ get a list of all Nets
