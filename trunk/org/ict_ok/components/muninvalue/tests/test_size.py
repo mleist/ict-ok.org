47efff806d5138db0da611e38ecb811ece909e9d
Markus Leist <markus@lei.st>
2008-09-24 16:20:12 +0000

    copy from already cleaned latency will be better
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@329 befa3f1b-49e7-4dde-a7c4-38a0be74952b

---
+++ trunk/org/ict_ok/components/muninvalue/tests/test_size.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id$
+#
+# pylint: disable-msg=W0221
+#
+
+__version__ = "$Id$"
+
+# python imports
+import unittest
+
+# zope imports
+
+# ict_ok.org imports
+
+def test_suite():
+    suite = unittest.TestSuite()
+    return suite
