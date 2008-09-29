b52caa980e4eab85f566da169bfbd006d68bfbcb
Markus Leist <markus@lei.st>
2008-09-29 09:29:36 +0000

    first copy of the smokeping utility
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@338 befa3f1b-49e7-4dde-a7c4-38a0be74952b

---
+++ trunk/org/ict_ok/admin_utils/generators/smokeping/interfaces.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id$
+#
+# pylint: disable-msg=E0213,E0211,W0232,W0622
+#
+"""Interface to nagios generator"""
+
+__version__ = "$Id$"
+
+# zope imports
+from zope.interface import Interface
+from zope.schema import Datetime, TextLine, Bool
+from org.ict_ok.schema.datetime import IctDatetime
+from zope.i18nmessageid import MessageFactory
+
+# ict_ok.org imports
+from org.ict_ok.admin_utils.generators.interfaces import \
+     IAdmUtilGenerators
+
+_ = MessageFactory('org.ict_ok')
+
+
+class IAdmUtilGeneratorNagios(IAdmUtilGenerators):
+    """
+    major component for registration and event distribution 
+    """
+    isRunning = Bool(
+        title = _("nagios is running"),
+        default = False)
+
+    pathInitScript = TextLine(
+        max_length = 200,
+        title = _(u"init script"),
+        description = _(u"nagios init script with path"),
+        default = _(u"/etc/init.d/nagios"),
+        required = True)
+
+    pathConfigData = TextLine(
+        max_length = 200,
+        title = _(u"path to configuration"),
+        default = u"/opt/nagios/etc/ict_ok",
+        required = True)
+
+    lastConfigFileChange = IctDatetime(
+        title=_(u"last change"),
+        description=_(u"last change of configuration file"),
+        required=False)
+
+    lastDeamonReload = IctDatetime(
+        title=_(u"last reload"),
+        description=_(u"last reload of nagios deamon"),
+        required=False)
+
+    def touchLastConfigFile():
+        """change timestamp in the utility
+        
+        will trigger a reload of the nagios-daemon on cron
+        """
+
+    def reloadDaemon():
+        """ reload the nagios daemon
+        """
+
+    def allConfigFilesOut():
+        """make all configuration files
+        """
+
+
+class IGenNagios(Interface):
+    """Interface of nagios-Adapter
+    """
+    def fileOpen():
+        """will open a filehandle to the specific object
+        """
+
+    def fileClose():
+        """will close the filehandle to the specific object
+        """
+
+    def write(text_arg):
+        """will write the text_arg into the configuration file
+        """
+
+    def traverse4nagiosGeneratorPre(level, comments):
+        """graphviz configuration preamble
+        
+        level: indent-level
+        comments: should there comments are in the output?
+
+        """
+
+    def traverse4nagiosGeneratorPost(level, comments):
+        """graphviz configurations text after object
+        
+        level: indent-level
+        comments: should there comments are in the output?
+
+        """
+
+    def traverse4nagiosGeneratorBody(level, comments):
+        """graphviz configuration data of/in object
+        
+        level: indent-level
+        comments: should there comments are in the output?
+
+        """
+
+    def traverse4nagiosGenerator(level, comments):
+        """Configuration generator
+        
+        level: indent-level
+        comments: should there comments are in the output?
+
+        """
+
+    def nagiosConfigFileOut(forceOutput=False, event=None):
+        """Nagios-Filegenerator
+        
+        will produce the nagios configuration files
+        
+        forceOutput: False will check for a relevant attribute change
+        True will alway generate a new config file
+         
+        event: None or the zope event from lifecycle
+       """
+
+    def nagiosConfigFileRemove():
+        """remove old nagios configuration file for this object
+        """
+
+    def eventModifiesCfgFile(event):
+        """check for attribute changes
+        """
+
---
+++ trunk/org/ict_ok/admin_utils/generators/smokeping/nagios.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id$
+#
+# pylint: disable-msg=E1101,E0611
+#
+"""Configuration adapter for nagios-config files
+"""
+
+__version__ = "$Id$"
+
+# python imports
+import os
+import logging
+import datetime
+
+# zope imports
+from zope.app import zapi
+from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
+from zope.component import getUtility
+
+# ict_ok.org imports
+from org.ict_ok.components.supernode.interfaces import ISupernode
+from org.ict_ok.admin_utils.generators.nagios.interfaces import \
+     IAdmUtilGeneratorNagios, IGenNagios
+from org.ict_ok.admin_utils.generators.generators import \
+     AdmUtilGenerators
+
+logger = logging.getLogger("AdmUtilGeneratorNagios")
+
+
+class AdmUtilGeneratorNagios(AdmUtilGenerators):
+    """Implementation of nagios configuration generator
+    """
+    implements(IAdmUtilGeneratorNagios)
+    
+    isRunning = FieldProperty(\
+        IAdmUtilGeneratorNagios['isRunning'])
+    lastConfigFileChange = FieldProperty(\
+        IAdmUtilGeneratorNagios['lastConfigFileChange'])
+    pathInitScript = FieldProperty(\
+        IAdmUtilGeneratorNagios['pathInitScript'])
+    pathConfigData = FieldProperty(\
+        IAdmUtilGeneratorNagios['pathConfigData'])
+    lastDeamonReload = FieldProperty(\
+        IAdmUtilGeneratorNagios['lastDeamonReload'])
+
+    def __init__(self):
+        self.isRunning = False
+        self.lastConfigFileChange = None
+        self.lastDeamonReload = None
+        self.pathInitScript = u"/etc/init.d/nagios"
+        self.pathConfigData = u"/opt/nagios/etc/ict_ok"
+        AdmUtilGenerators.__init__(self)
+        self.ikRevision = __version__
+
+    def touchLastConfigFile(self):
+        """change timestamp in the utility
+        
+        will trigger a reload of the nagios-daemon on cron
+        """
+        self.lastConfigFileChange = datetime.datetime.utcnow()
+        
+    def reloadDaemon(self):
+        """ reload the nagios daemon
+        """
+        nagiosUtil = getUtility(IAdmUtilGeneratorNagios)
+        if nagiosUtil is not None and \
+           nagiosUtil.isRunning:
+            retInt = os.system('echo "" |sudo -S ' + self.pathInitScript + \
+                               ' reload > /dev/null 2>&1')
+            if retInt == 0:
+                self.lastDeamonReload = datetime.datetime.utcnow()
+            else:
+                logger.warning(u"reloadDaemon Error No: %d", retInt)
+        
+    def allConfigFilesOut(self):
+        """make configuration file
+        """
+        self.touchLastConfigFile()
+        its = zapi.getRoot(self).items()
+        for (dummy_name, oobj) in its:
+            if ISupernode.providedBy(oobj):
+                try:
+                    adapterGenNagios = IGenNagios(oobj)
+                    if adapterGenNagios:
+                        adapterGenNagios.nagiosConfigFileOut(True, None)
+                except TypeError, errText:
+                    logger.error(u"Problem in adaption of nagios config: %s" %\
+                                 (errText))
+
+    def tickerEvent(self):
+        """check for changes
+        """
+        if self.lastDeamonReload is None:
+            self.reloadDaemon()
+        elif self.lastConfigFileChange >= self.lastDeamonReload:
+            self.reloadDaemon()
