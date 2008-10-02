8d99bef514c1eb7d72658c48bc6eef087e9f02ea
Markus Leist <markus@lei.st>
2008-10-02 03:06:08 +0000

    cleanup for interface object
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@341 befa3f1b-49e7-4dde-a7c4-38a0be74952b

---
+++ trunk/org/ict_ok/components/interface/adapter/smokeping.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2004, 2005, 2006, 2007, 2008,
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id$
+#
+# pylint: disable-msg=E1101,E0611
+#
+"""Adapter implementation for generating graphviz-dot configuration
+"""
+
+__version__ = "$Id$"
+
+# python imports
+import logging
+
+# zope imports
+from zope.interface import implements
+from zope.component import adapts
+
+# ict_ok.org imports
+from org.ict_ok.components.interface.interfaces import IInterface
+from org.ict_ok.components.supernode.adapter.smokeping import \
+     GenSmokePing as ParentGenSmokePing
+from org.ict_ok.admin_utils.generators.smokeping.interfaces import \
+     IGenSmokePing
+
+logger = logging.getLogger("HostGenSmokePing")
+
+
+class GenSmokePing(ParentGenSmokePing):
+    """adapter implementation of Interface -> smokeping
+    """
+
+    implements(IGenSmokePing)
+    adapts(IInterface)
+    
+    # modification of this attributes will trigger an new generation of
+    # the config file
+    attrList = ['ipv4List']
+    
+    def traverse4smokepingGeneratorPre(self, level=0, comments=True):
+        """graphviz configuration preamble
+        """
+        if comments:
+            self.write(u"%s## Pre (%s,%d) - InterfaceGenSmokePing" % \
+                       ("\t" * level, self.context.ikName, level))
+        self.write(u"+++ %s\n" % (self.context.objectID))
+        self.write(u"menu = %s\n" % (self.context.ikName))
+        self.write(u"title = %s\n" % (self.context.ikName))
+        return True # valueChanged
+
+    def traverse4smokepingGeneratorBody(self, level=0, comments=True):
+        """graphviz configuration data of/in object
+        """
+        ParentGenSmokePing.traverse4smokepingGeneratorBody(self,
+                                                           level,
+                                                           comments)
+        return True # valueChanged
