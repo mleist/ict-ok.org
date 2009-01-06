886d1dba8d3bf9539ffc7db3c703e43c6d13320a
Markus Leist <markus@lei.st>
2009-01-06 15:12:30 +0000

    first cog-driven component template
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@394 befa3f1b-49e7-4dde-a7c4-38a0be74952b

---
+++ branches/V9.04/org/ict_ok/cog_templates/components/template/props.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2007,
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id$
+#
+
+authors = [
+   {r'name': r'Markus Leist',
+    r'email': r'leist@ikom-online.de'},
+    ]
+
+filename = r'test'
+longpath = r'org.ict_ok.components'
+longpath_interface = longpath + r'.%s.interfaces' % (filename)
+longpath_file = longpath + r'.%s.%s' % (filename, filename)
+
+moduletitle = r'Test Instance'
+componentname = r'Test'
+#servicename = r'Test'
+#serviceport = 65533
+#utilityname = r'AdmUtil' + modulename
+#loggername = r'AdmUtil' + modulename
+#servicetitle = r'%s Service' % servicename
+
+purpose = r"""test component
+
+A test component will server some tests ...
+"""
+
+copyrights = [2006, 2007, 2008, 2009]
