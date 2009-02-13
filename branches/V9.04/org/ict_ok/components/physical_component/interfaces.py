62ea4cdbed5aa9216c920ed5a686a7d43d7f086d
Markus Leist <markus@lei.st>
2009-02-13 10:40:55 +0000

    svn duplicate
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@444 befa3f1b-49e7-4dde-a7c4-38a0be74952b

---
+++ branches/V9.04/org/ict_ok/components/physical_component/interfaces.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2008, 2009, 
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id: interfaces.py_cog 399 2009-01-08 14:00:17Z markusleist $
+#
+# pylint: disable-msg=E0213,W0232
+#
+"""Interface of PhysicalConnector"""
+
+
+__version__ = "$Id: interfaces.py_cog 399 2009-01-08 14:00:17Z markusleist $"
+
+# zope imports
+from zope.interface import Interface
+from zope.i18nmessageid import MessageFactory
+from zope.schema import Choice, Int, List, TextLine
+from zope.app.folder.interfaces import IFolder
+
+# ict_ok.org imports
+from org.ict_ok.components.superclass.interfaces import ISuperclass
+from org.ict_ok.components.interfaces import IComponent
+
+_ = MessageFactory('org.ict_ok')
+
+
+class IPhysicalConnector(IComponent):
+    """A PhysicalConnector object."""
+
+    connectorPinout = TextLine(
+        max_length = 80,
+        title = _("Connector pinout"),
+        required = False)
+
+    room = Choice(
+        title=_(u'Room'),
+        vocabulary='AllRooms',
+        required=False
+        )
+    
+    interface = Choice(
+        title=_(u'Connected interface'),
+        vocabulary='AllUnusedOrUsedPhysicalConnectorInterfaces',
+        required=False
+        )
+
+
+
+class IPhysicalConnectorFolder(ISuperclass, IFolder):
+    """Container for PhysicalConnector objects
+    """
+
+
+class IAddPhysicalConnector(Interface):
+    """Interface for all Objects"""
+    template = Choice(
+        title = _("Template"),
+        vocabulary="AllPhysicalConnectorTemplates",
+        required = False)
---
+++ branches/V9.04/org/ict_ok/components/physical_component/physical_connector.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2008, 2009, 
+#               Markus Leist <leist@ikom-online.de>
+# See also LICENSE.txt or http://www.ict-ok.org/LICENSE
+# This file is part of ict-ok.org.
+#
+# $Id: template.py_cog 399 2009-01-08 14:00:17Z markusleist $
+#
+# pylint: disable-msg=E1101,W0142
+#
+"""implementation of PhysicalConnector"""
+
+__version__ = "$Id: template.py_cog 399 2009-01-08 14:00:17Z markusleist $"
+
+# python imports
+
+# zope imports
+from zope.interface import implements
+from zope.schema.fieldproperty import FieldProperty
+from zope.app.folder import Folder
+
+# lovely imports
+from lovely.relation.property import RelationPropertyIn
+from lovely.relation.property import RelationPropertyOut
+from lovely.relation.property import FieldRelationManager
+
+# ict_ok.org imports
+from org.ict_ok.libs.lib import getRefAttributeNames
+from org.ict_ok.components.superclass.superclass import Superclass
+from org.ict_ok.components.physical_connector.interfaces import \
+    IPhysicalConnector, IPhysicalConnectorFolder, IAddPhysicalConnector
+from org.ict_ok.components.interfaces import \
+    IImportCsvData, IImportXlsData
+from org.ict_ok.components.component import Component
+from org.ict_ok.components.interface.interfaces import IInterface
+from org.ict_ok.components.component import \
+    AllComponents, AllComponentTemplates, AllUnusedOrSelfComponents
+from org.ict_ok.components.room.room import Room_PhysicalConnectors_RelManager
+
+def AllPhysicalConnectorTemplates(dummy_context):
+    return AllComponentTemplates(dummy_context, IPhysicalConnector)
+
+def AllPhysicalConnectors(dummy_context):
+    return AllComponents(dummy_context, IPhysicalConnector)
+
+def AllUnusedOrUsedRoomPhysicalConnectors(dummy_context):
+    return AllUnusedOrSelfComponents(dummy_context, IPhysicalConnector, 'room')
+
+def AllUnusedOrUsedInterfacePhysicalConnectors(dummy_context):
+    return AllUnusedOrSelfComponents(dummy_context, IPhysicalConnector, 'interface')
+
+PhysicalConnector_Interface_RelManager = \
+    FieldRelationManager(IPhysicalConnector['interface'],
+                         IInterface['physicalConnector'],
+                         relType='physicalConnector:interface')
+
+
+class PhysicalConnector(Component):
+    """
+    the template instance
+    """
+    implements(IPhysicalConnector)
+    shortName = "physical_connector"
+    # for ..Contained we have to:
+    __name__ = __parent__ = None
+    
+    connectorPinout = FieldProperty(IPhysicalConnector['connectorPinout'])
+    
+    room = RelationPropertyIn(Room_PhysicalConnectors_RelManager)
+
+    interface = RelationPropertyIn(PhysicalConnector_Interface_RelManager)
+
+    fullTextSearchFields = ['connectorPinout']
+    fullTextSearchFields.extend(Component.fullTextSearchFields)
+
+    def __init__(self, **data):
+        """
+        constructor of the object
+        """
+        Component.__init__(self, **data)
+        refAttributeNames = getRefAttributeNames(PhysicalConnector)
+        for (name, value) in data.items():
+            if name in IPhysicalConnector.names():
+                if name not in refAttributeNames:
+                    setattr(self, name, value)
+        self.ikRevision = __version__
+
+    def store_refs(self, **data):
+        refAttributeNames = getRefAttributeNames(PhysicalConnector)
+        for (name, value) in data.items():
+            if name in refAttributeNames:
+                setattr(self, name, value)
+
+
+class PhysicalConnectorFolder(Superclass, Folder):
+    implements(IPhysicalConnectorFolder, 
+               IImportCsvData,
+               IImportXlsData,
+               IAddPhysicalConnector)
+    def __init__(self, **data):
+        """
+        constructor of the object
+        """
+        Superclass.__init__(self, **data)
+        Folder.__init__(self)
