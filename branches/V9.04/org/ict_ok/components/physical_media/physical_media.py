507360a8e7a729356d7f8b63a877342a34bccd75
Markus Leist <markus@lei.st>
2009-06-04 15:53:28 +0000

    some more info on AllPhysicalMedia
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@546 befa3f1b-49e7-4dde-a7c4-38a0be74952b

--- branches/V9.04/org/ict_ok/components/physical_media/physical_media.py
+++ branches/V9.04/org/ict_ok/components/physical_media/physical_media.py
@@ -50,7 +50,8 @@ def AllPhysicalMediaTemplates(dummy_context):
     return AllComponentTemplates(dummy_context, IPhysicalMedia)
 
 def AllPhysicalMedia(dummy_context):
-    return AllComponents(dummy_context, IPhysicalMedia)
+    return AllComponents(dummy_context, IPhysicalMedia,
+                         additionalAttrNames=['device'])
 
 def AllUnusedOrUsedDevicePhysicalMedia(dummy_context):
     return AllUnusedOrSelfComponents(dummy_context, IPhysicalMedia, 'device')
