65df63932d13a2c98b3af154051bc75324a2098c
Sebastian Napiorkowski <>
2009-06-22 11:10:23 +0000

    improvement
    git-svn-id: file:///opt/NoBackup/svn_ict-ok_ddd@555 befa3f1b-49e7-4dde-a7c4-38a0be74952b

--- branches/V9.04/org/ict_ok/libs/mac_db.py
+++ branches/V9.04/org/ict_ok/libs/mac_db.py
@@ -24,38 +24,41 @@ class MacDB():
         self._data = IOBTree()
         last_mac = None
         EOF = False
-        re_mac = re.compile(r"[A-Z0-9]*\-[A-Z0-9]*\-[A-Z0-9]*")
+        re_mac = re.compile(r"[A-F0-9]*\-[A-F0-9]*\-[A-F0-9]*")
         while not EOF:
             line = f.readline()
             if re.match(re_mac, line):
                 last_mac = int(line[:8].replace("-", ""), 16)
-                index = line.rfind("\t")
-                org = line[index+1:].strip()
+                index = line.rfind("\t")+len("\t")
+                org = line[index:].strip()
                 self._data[last_mac] = {"short": org, "other":[]}
             elif last_mac != None:
                 index = line.rfind("\t")
-                if index != None and line != "\n":
-                    other = line[index+1:].strip()
+                if index != -1:
+                    index += len("\t")
+                    other = line[index:].strip()
                     self._data[last_mac]["other"].append(other)
                 if (line == "\n"):
-                    country_index = len(self._data[last_mac]["other"])-1
+                    country_index = len(self._data[last_mac]["other"])-1 #last element
                     self._data[last_mac]["country"] = self._data[last_mac]["other"][country_index]
                     del self._data[last_mac]["other"][country_index]
             if line == '':
                 EOF=True
         f.close()
-        
-    
+
+
     def getOrganization(self, mac_string):
-        int_mac = int(mac_string[:8].replace(":", ""), 16)
+        mac = mac_string[:8].replace(":", "")
+        mac = mac.replace("-", "")
+        int_mac = int(mac, 16)
         try:
             ret = self._data[int_mac]
         except KeyError:
             ret = None
         return ret
-        
+
 
 #macUtil = MacDB()
 #mac_list = ["00:04:75:d5:2f:11 ", "00:11:2f:77:f6:ef", "00:11:2f:77:f6:ef", "00:50:56:c0:00:01", "00:0d:88:53:3a:4e", "00:1e:8c:7c:7c:97",  "00:1b:21:0f:f6:8e"]
 #for mac in mac_list:
-    #print macUtil.getOrganization(mac)
+#    print macUtil.getOrganization(mac)
